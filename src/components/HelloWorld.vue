41e8040fcd5d298e3b2ee0672347d570b7328ec8
dbd <276989756@qq.com>
2019-12-19 09:24:07 +0800

    First commit;

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-radio v-model="radio" label="1">备选项</el-radio>
+    <el-radio v-model="radio" label="2">备选项</el-radio>
+  </el-container>
+</template>
+
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
+
+<script>
+export default {
+  data() {
+ 
+    return {
+      radio:'1'
+    };
+  }
+};
+</script>
